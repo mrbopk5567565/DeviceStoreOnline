b9a32d4cb28a4891a06af7152f51a23db90252c3
Phan Nguyen Truong Thinh <51321217+mrbopk5567565@users.noreply.github.com>
2019-10-14 15:13:57 +0700

    ActionViewFlipper

--- app/src/main/java/thinhtien/pntt/phannguyentruongthinh/onlinedevicestore/activity/MainActivity.java
+++ app/src/main/java/thinhtien/pntt/phannguyentruongthinh/onlinedevicestore/activity/MainActivity.java
@@ -8,10 +8,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ViewFlipper;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 import thinhtien.pntt.phannguyentruongthinh.onlinedevicestore.R;
 
@@ -31,6 +37,29 @@ public class MainActivity extends AppCompatActivity {
 
         AnhXa();
         ActionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionViewFlipper() {
+        ArrayList<String> mangQuangCao = new ArrayList<>();
+        mangQuangCao.add("https://cdn.tgdd.vn/Products/Images/42/209535/xiaomi-redmi-note-8-white-18thangbh-400x460.png");
+        mangQuangCao.add("https://cdn.tgdd.vn/Products/Images/42/211161/realme-5-4gb-purple-docquyen-400x460.png");
+        mangQuangCao.add("https://cdn.tgdd.vn/Products/Images/42/182153/oppo-f9-tim-400x460.png");
+        mangQuangCao.add("https://cdn.tgdd.vn/Products/Images/42/192003/samsung-galaxy-a9-2018-blue-400x460.png");
+        for (int i = 0; i < mangQuangCao.size(); i++) {
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext())
+                    .load(mangQuangCao.get(i))
+                    .into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            mViewFlipper.addView(imageView);
+        }
+        mViewFlipper.setFlipInterval(5000);
+        mViewFlipper.setAutoStart(true);
+        Animation animation_slide_in = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        Animation animation_slide_out = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.silde_out_right);
+        mViewFlipper.setInAnimation(animation_slide_in);
+        mViewFlipper.setOutAnimation(animation_slide_out);
     }
 
     private void ActionBar() {
